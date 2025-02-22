338c8fb34b16bccb71356c814a8d75fd2b28acd1
JimboRumbleCadumbo <1297275552jimjim@gmail.com>
2025-02-21 23:25:38 -0800

    changed page folder name to 'templates' for flask

--- application/main/main.py
+++ application/main/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('pages/index.html')  # Render the index.html file
+    return render_template('index.html')  # Render the index.html file
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Default Flask server (not for production)
