de546fe73c551bc9f1ada51ed8e4344cdc4b23ca
JimboRumbleCadumbo <1297275552jimjim@gmail.com>
2025-02-22 16:47:22 -0800

    made link to other pages

--- application/main/main.py
+++ application/main/main.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')  # Render the index.html file
 
+@app.route('/about/alexis')
+def about_alexis():
+    return render_template('about-alexis.html') # Render the about_alexis.html file
+
+@app.route('/about/david')
+def about_david():
+    return render_template('about-david.html') # Render the about_alexis.html file
+
+@app.route('/about/jun')
+def about_jun():
+    return render_template('about-jun.html') # Render the about_alexis.html file
+
+@app.route('/about/yuming')
+def about_yuming():
+    return render_template('about-yuming.html') # Render the about_alexis.html file
+
+@app.route('/about/athan')
+def about_athan():
+    return render_template('about-athan.html') # Render the about_alexis.html file
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Default Flask server (not for production)
