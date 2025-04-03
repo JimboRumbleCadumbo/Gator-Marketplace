b4f050b8ab50c9e994e41173fdca166f744c223a
alexis perez <aperez75@sfsu.edu>
2025-04-02 19:04:47 -0700

    Added the cards for the search

--- application/main/static/pages/Home.js
+++ application/main/static/pages/Home.js
@@ -6,6 +6,36 @@ export default {
         <div class="container">
             <div class="content">
                 <h1>CSC 648-Spring 2025 Team 05</h1>
+
+                <div class="product-grid">
+                    <div class="card">
+                        <img src="https://via.placeholder.com/150" alt="Item Image" />
+                        <h3>Product Name</h3>
+                        <p>Product description</p>
+                    </div>
+
+                    <div class="card">
+                        <img src="" alt="Item Image" />
+                        <h3>Product Name</h3>
+                        <p>Product description</p>
+                    </div>
+
+                    <div class="card">
+                        <img src="" alt="Item Image" />
+                        <h3>Product Name</h3>
+                        <p>Product description</p>
+                    </div>
+                    <div class="card">
+                        <img src="" alt="Item Image" />
+                        <h3>Product Name</h3>
+                        <p>Product description</p>
+                    </div>
+                    <div class="card">
+                        <img src="" alt="Item Image" />
+                        <h3>Product Name</h3>
+                        <p>Product description</p>
+                    </div>
+                </div>
                 
             </div>
         </div>
