1bf276a52b6634721860f720ab921f7efb0c3b27
JimboRumbleCadumbo <1297275552jimjim@gmail.com>
2025-04-07 18:10:05 -0700

    fixed missing search button

--- application/main/__init__.py
+++ application/main/__init__.py
@@ -6,47 +6,12 @@ __version__ = "0.1.0"
 
 app = Flask(__name__)
 
-app.jinja_options = {
-    'variable_start_string': '[[',
-    'variable_end_string': ']]',
-    'block_start_string': '{%',
-    'block_end_string': '%}'
-}
-
 # Initialize routes from search module
 search.init_search_routes(app)
 
-# Search API route for Vue to fetch results
-@app.route('/api/search', methods=['POST'])
-def api_search():
-    data = request.get_json()
-    query = data.get('query', '')
-    filter_by = data.get('filter', '')
-
-@app.route('/about/david')
-def about_david():
-    return render_template('about-david.html') # Render the about_david.html file
-
-@app.route('/about/jun')
-def about_jun():
-    return render_template('about-jun.html') # Render the about_jun.html file
-
-@app.route('/about/yuming')
-def about_yuming():
-    return render_template('about-yuming.html') # Render the about-yuming.html file
-
-@app.route('/about/athan')
-def about_athan():
-    return render_template('about-athan.html') # Render the about-athan.html file
-
 @app.route('/')
 def index():
-    return render_template('home_pagevp.html')  # Render the index.html file
-
-@app.route('/test/search-test')
-def search_test():
-    return render_template('search-test.html')  # Render the search_test.html file
-
+    return render_template('index.html')  # Render the index.html file
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Default Flask server (not for production)
--- application/main/static/searchbar.js
+++ application/main/static/searchbar.js
@@ -1,14 +1,27 @@
 
 export default{
     template: `
-        <div class="search-bar-container">
-            <!-- Filters Dropdown -->
-            <div class="dropdown" :class="{ open: isDropdownVisible }">
-                <button @click="toggleDropdown" class="dropbtn">{{ selectedFilter || "All" }}</button>
-                <div class="dropdown-content" v-if="isDropdownVisible">
-                    <div v-for="filter in filters" :key="filter" @click="selectFilter(filter)">
-                        {{ filter }}
+        <!-- Search Bar and Filters Dropdown in Flexbox -->
+            <div class="search-bar-container">
+                <!-- Filters Dropdown -->
+                <div class="dropdown" :class="{ open: isDropdownVisible }">
+                    <button @click="toggleDropdown" class="dropbtn">{{ selectedFilter || "All" }}</button>
+                    <div class="dropdown-content">
+                        <div v-for="filter in filters" :key="filter" @click="selectFilter(filter)">
+                            {{ filter }}
+                        </div>
                     </div>
+                </div>
+                
+                <!-- Search Bar -->
+                <div class="search-bar">
+                    <input 
+                        type="text" 
+                        v-model="searchQuery" 
+                        placeholder="Search..." 
+                        @keyup.enter="submitSearch" 
+                    />
+                    <button @click="submitSearch">Search</button>
                 </div>
             </div>
             `,
