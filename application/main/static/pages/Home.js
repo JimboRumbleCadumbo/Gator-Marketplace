d56c2fb303ed90735dde48406701b2fda3beef62
davidcabanela <dcabanel@mail.sfsu.edu>
2025-04-20 03:44:40 -0700

    replaced navbar component in home page

--- application/main/static/pages/Home.js
+++ application/main/static/pages/Home.js
@@ -1,13 +1,11 @@
 console.log("Home.js Loaded!");
 
+import Navbar from "/static/components/Navbar.js";
+
 export default {
+    components: { Navbar },
     template: `
-        <nav class="nav-bar">
-            <router-link to="/" class="nav-link">Home</router-link>
-            <search-bar></search-bar>
-            <router-link to="/postings" class="nav-link">Create Listing</router-link>
-        </nav>
-        
+        <Navbar />
         <div class="container">
             <div class="content">
                 <h1>CSC 648-Spring 2025 Team 05</h1>
