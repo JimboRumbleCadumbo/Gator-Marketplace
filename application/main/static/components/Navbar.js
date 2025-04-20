e1bf9bc4fd4152339f8d406c125a3a6b0f06aa34
davidcabanela <dcabanel@mail.sfsu.edu>
2025-04-20 04:01:59 -0700

    changed name of tab from Sell to Create Listing

--- application/main/static/components/Navbar.js
+++ application/main/static/components/Navbar.js
@@ -3,7 +3,7 @@ export default {
         <nav class="nav-bar">
             <router-link to="/" class="nav-link">Home</router-link>
             <search-bar></search-bar>
-            <router-link to="/postings" class="nav-link">Sell</router-link>
+            <router-link to="/postings" class="nav-link">Create Listing</router-link>
             <router-link to="/login" class="nav-link">Login</router-link>
             <router-link to="/signup" class="nav-link">Sign Up</router-link>
         </nav>
