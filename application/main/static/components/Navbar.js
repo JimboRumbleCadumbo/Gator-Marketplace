28bbb93202df4e5e110d8c23e26c74306f9ec59b
davidcabanela <dcabanel@mail.sfsu.edu>
2025-04-20 03:43:52 -0700

    created new reusable navbar component

---
+++ application/main/static/components/Navbar.js
@@ -0,0 +1,11 @@
+export default {
+    template: `
+        <nav class="nav-bar">
+            <router-link to="/" class="nav-link">Home</router-link>
+            <search-bar></search-bar>
+            <router-link to="/postings" class="nav-link">Sell</router-link>
+            <router-link to="/login" class="nav-link">Login</router-link>
+            <router-link to="/signup" class="nav-link">Sign Up</router-link>
+        </nav>
+    `,
+};
