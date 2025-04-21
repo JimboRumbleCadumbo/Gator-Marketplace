3d96a7c7f8d58052db4bd064bf5b15feab2c7371
JunN0tJune <dchang6@mail.sfsu.edu>
2025-04-20 20:26:02 -0700

    created template for login.js

--- application/main/static/login.js
+++ application/main/static/login.js
@@ -1,3 +1,23 @@
+export default {
+  template: `
+    <div id="login-form">
+      <h1>SFSU Login</h1>
+      <form @submit.prevent="handleLogin">
+        <label for="login-email">SFSU Email</label>
+        <input type="email" id="login-email" v-model="email" required />
+
+        <label for="login-password">Password</label>
+        <input type="password" id="login-password" v-model="password" required />
+
+        <button type="submit">Login</button>
+
+        <p class="toggle-link">
+          <router-link to="/signup">Don't have an account? Sign up</router-link>
+        </p>
+      </form>
+    </div>
+  `, 
+}
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.getElementById("login-form");
 
