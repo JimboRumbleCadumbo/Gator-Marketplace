42ad2b0429a8d8e0b050460d7d141dab50f27551
davidcabanela <dcabanel@mail.sfsu.edu>
2025-05-04 02:14:02 -0700

    Added dynamic rendering of login/logout buttons to navbar based on whether user is logged in

--- application/main/static/components/Navbar.js
+++ application/main/static/components/Navbar.js
@@ -6,7 +6,33 @@ export default {
     return {
       // You can replace this with a real user icon URL or a computed property
       userIcon: "https://api.dicebear.com/8.x/bottts/svg?seed=CoolUser123",
+      loggedIn: false,
+      userName: "",
     };
+  },
+  methods: {
+    initializeLoginState() {
+        const loginState = window.__LOGIN_STATE__; // Injected by the backend
+        if (loginState) {
+            this.loggedIn = loginState.logged_in;
+            this.userName = loginState.user_name || "";
+        }
+    },
+    handleLogout() {
+      fetch('/api/logout', { method: 'POST' })
+        .then(() => {
+          this.loggedIn = false;
+          this.userName = "";
+          alert("Logged out successfully");
+          window.location.href = '/';
+        })
+        .catch((error) => {
+          console.error("Logout error:", error);
+        });
+    },
+  },
+  mounted() {
+    this.initializeLoginState();
   },
   template: `
         <nav class="nav-bar">
@@ -16,10 +42,12 @@ export default {
             </div>
             <Searchbar></Searchbar>
             <router-link to="/postings" class="nav-link">Post</router-link>
-            <router-link to="/login" class="nav-link">Login/Signup</router-link>
-            <router-link to="/profile" class="nav-link profile-icon-link">
+            <router-link v-if="!loggedIn" to="/login" class="nav-link">Login/Signup</router-link>
+            <router-link v-if="loggedIn" to="/profile" class="nav-link profile-icon-link">
                 <img :src="userIcon" alt="Profile" class="nav-profile-icon" />
+                <span>Welcome, {{ userName }}</span>
             </router-link>
+            <button v-if="loggedIn" @click="handleLogout" class="nav-link">Logout</button>
         </nav>
     `,
 };
