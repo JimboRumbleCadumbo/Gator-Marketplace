b98ff5b9d5b34cb0bb7d476e100df93706b017af
alexis perez <aperez75@sfsu.edu>
2025-05-05 22:20:27 -0700

    Added chagnes to navbar

--- application/main/static/components/Navbar.js
+++ application/main/static/components/Navbar.js
@@ -8,6 +8,7 @@ export default {
       userIcon: "https://api.dicebear.com/8.x/bottts/svg?seed=CoolUser123",
       loggedIn: false,
       userName: "",
+      showDropdown: false,
     };
   },
   methods: {
@@ -30,24 +31,41 @@ export default {
           console.error("Logout error:", error);
         });
     },
+    toggleDropdown() {
+        this.showDropdown = !this.showDropdown;
+    },
   },
   mounted() {
     this.initializeLoginState();
   },
   template: `
-        <nav class="nav-bar">
+    <nav class="nav-bar">
+        <div class="nav-left">
             <router-link to="/" class="nav-link">Gator Savvy</router-link>
             <div class="demonstration-only">
-                SFSU Software Engineering Project<br />CSC 648-848, Spring 2025.<br />For Demonstration Only
+            SFSU Software Engineering Project<br />
+            CSC 648-848, Spring 2025.<br />
+            For Demonstration Only
             </div>
-            <Searchbar></Searchbar>
-            <router-link to="/postings" class="nav-link">Post</router-link>
+        </div>
+
+        <div class="nav-center">
+            <Searchbar> </Searchbar>
+        </div>
+
+        <div class="nav-right">
+            <router-link v-if="loggedIn" to="/postings" class="nav-link">Post</router-link>
             <router-link v-if="!loggedIn" to="/login" class="nav-link">Login/Signup</router-link>
-            <router-link v-if="loggedIn" to="/profile" class="nav-link profile-icon-link">
+
+            <div v-if="loggedIn" class="nav-link dropdown" @click="toggleDropdown">
+                <span class="dropdown-label">Welcome, {{ userName }}</span>
                 <img :src="userIcon" alt="Profile" class="nav-profile-icon" />
-                <span>Welcome, {{ userName }}</span>
-            </router-link>
-            <button v-if="loggedIn" @click="handleLogout" class="nav-link">Logout</button>
-        </nav>
+                    <div v-if="showDropdown" class="dropdown-menu">
+                        <router-link to="/profile" class="dropdown-item">Profile</router-link>
+                        <button @click.stop="handleLogout" class="dropdown-item">Logout</button>
+                    </div>
+            </div>
+        </div>
+    </nav>
     `,
 };
